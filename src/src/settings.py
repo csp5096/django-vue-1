b9a8db184d4bc409d6d19df1642711b1cfc06cc1
csp5096 <csp5096@yahoo.com>
2017-11-27 20:21:48 -0600

    API 1

--- src/src/settings.py
+++ src/src/settings.py
@@ -44,7 +44,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dotenv',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
