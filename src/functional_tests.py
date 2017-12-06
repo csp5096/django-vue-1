c75b3bd1fc60c39b8659cc544395745b863fa6b8
csp5096 <csp5096@yahoo.com>
2017-12-06 17:07:43 -0600

    Lists 5

--- src/functional_tests.py
+++ src/functional_tests.py
@@ -11,6 +11,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        "Hepler Method"
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out it homepage
@@ -37,38 +43,49 @@ class NewVisitorTest(unittest.TestCase):
         # She types "Buy peacock feathers" in a text box
         # Edith's hobby is typing fly-fishing lures
 
-        # form_textfield = driver.find_element_by_id('Buy peacock feathers')
+        # inputbox = self.browser.find_element_by_id('Buy peacock feathers')
 
         ## Where she hits enter, the page updates and lists
         # "1: Buy peacock feathers" as an item in a to-do list
 
-        #form_textfield.send_keys("")
-        # form_textfield.submti()
+        # input.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # There is still a text box inivting her to add another itme
         # She enters "Use peacock feathers to make a fly"
         # Edith is very methodical
 
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        # inputbox.send_keys('Use peacockfeather to make a fly')
+        # inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, an now shows both items on here list
+
+        self.check_for_row_in_list_table('1: Buy peacock fethers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn('1; Buy peacock feathers', [row.text for row in rows])
+        # self.assertIn('2: Use peacock featehrs to make a fly', [row.text for row in rows])
 
         # Edith wonders whether the site will remember here list.
         # Then she sees that the site has generated a unique URL for here --
         # there is some explanatory text to that effect.
 
+        self.fail('Finish the test!')
+
         # She visits that URL - here to-do list i still there.
 
         # Satisfied, she goes back to sleep
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
