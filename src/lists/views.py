03fbcb19f7ea50508544b6d5ecf4c6a8b131f515
csp5096 <csp5096@yahoo.com>
2017-12-07 17:35:42 -0600

    Home & Lists Pages

--- src/lists/views.py
+++ src/lists/views.py
@@ -7,14 +7,14 @@ def home_page(request):
     # item = Item()
     # item.text = request.POST.get('item_text', '')
     # item.save()
-    if request.method == 'POST':
+    """if request.method == 'POST':
         # new_item_text = request.POST['item_text']
         # Item.objects.create(text=new_item_text)
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')"""
 
-    items = Item.objects.all()
-    return render (request, 'home.html', {'items': items})
+    # items = Item.objects.all()
+    return render (request, 'home.html')
 
     # else:
         # new_item_text = ''
@@ -22,3 +22,11 @@ def home_page(request):
     # return render(request, 'home.html', {
         #'new_item_text': new_item_text,
     #})
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
+
+def new_lists(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
